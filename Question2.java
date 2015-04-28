e5f1b2b58870b6341e7aee5fe4c4caf9af483706
AM021355 <a0912481437@gmail.com>
2015-04-28 17:01:58 +0800

    A AM021355 簡弘翔提交 txtCopy

--- Question2.java
+++ Question2.java
@@ -1,7 +1,30 @@
-﻿public class Question2{
+﻿import java.io.File;
+import.java.io.IOException;
+
+public class Question2{
 	public static void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
 		editFile("Dest.txt");
 		updateFile("Dest.txt");
+	}
+}
+
+public static void txtCopy(String content,String temp)
+{
+	try
+	{
+		FileReader fr=new FileReader(content);
+		FileWriter fw=new FileWriter(temp);
+		BufferedReader br=new BufferedReader(fr);
+		String line;
+		while((line=br.readLine())!=null)
+		{
+		 	fw.write(line+"\r\n");
+		}
+			fw.close();
+	}
+	catch(Exception e)
+	{
+		System.out.println(e.toString());
 	}
 }
